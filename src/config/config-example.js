21455ad6f199d599605f0a51828f00af97a22916
Faress <faress5@hotmail.com>
2021-03-11 20:30:42 +0100

    update config-example.js

--- src/config/config-example.js
+++ src/config/config-example.js
@@ -1,3 +1,3 @@
-export const apiKey = 'your_api_key';
+export const POKEMON_TCG_API_KEY = 'your_api_key';
 // V2 by default
 export const POKEMON_TCG_ENDPOINT = 'https://api.pokemontcg.io/v2/cards';
